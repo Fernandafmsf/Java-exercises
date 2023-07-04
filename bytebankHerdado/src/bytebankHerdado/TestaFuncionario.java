bdd1bfa4e3883601a55f85bc40e6a5c1c42f002b
Fernandafmsf <fernandafernandesauto@gmail.com>
2023-07-04 15:36:25 -0300

    first commit of bytebankHerdado

---
+++ bytebankHerdado/src/bytebankHerdado/TestaFuncionario.java
@@ -0,0 +1,18 @@
+package bytebankHerdado;
+
+public class TestaFuncionario {
+
+	public static void main(String[] args) {
+		
+		Funcionario nico = new Funcionario();
+		
+		nico.setNome("Nico");
+		nico.setCpf("1242684");
+		nico.setSalario(2600.00);
+		
+		System.out.println(nico.getNome());
+		System.out.println(nico.getBonificacao());
+
+	}
+
+}
---
+++ bytebankHerdado/src/bytebankHerdado/TestaGerente.java
@@ -0,0 +1,24 @@
+package bytebankHerdado;
+
+public class TestaGerente {
+
+	public static void main(String[] args) {
+		Gerente g1 = new Gerente();
+		g1.setNome("Marcos");
+		g1.setCpf("1234684");
+		g1.setSalario(5000.0);
+		g1.setSenha(1234);
+		
+		boolean autentica = g1.autentica(1234);
+		
+		if(autentica) {
+			System.out.println(g1.getBonificacao());
+			System.out.println(g1.getCpf());
+			System.out.println(g1.getNome());
+		}else {
+			System.out.println("Senha inváida");
+		}
+
+	}
+
+}
